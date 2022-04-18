ca7a3bce2b83a2262eebd5c4a277d3a202239c41
skpanchal393 <skpanchal393@gmail.com>
2022-04-18 21:41:12 +0530

    Create Angular Application

---
+++ src/app/modules/front/index/component/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '../view/app.component.html',
+})
+export class AppComponent {
+  title = 'car-book-app';
+}
